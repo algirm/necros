575f24b764431314b9721c109892ddca4f598690
Algier <algier@eigenfinhub.com>
2024-02-04 23:13:35 +0700

    Update gradle plugin to 8.2.2

--- shared/common/build.gradle.kts
+++ shared/common/build.gradle.kts
@@ -120,10 +120,10 @@ android {
     namespace = "id.northbit.necros"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
+//    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+//    sourceSets["main"].res.srcDirs("src/androidMain/res")
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
+    
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
