5a4db331cd755a7bd089d7061b18fb9fb3a0bfc5
Algier <algier@eigenfinhub.com>
2024-02-11 20:00:20 +0700

    Fix sq file plugin sqldelight not sync

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.gradleUtil)
-    alias(libs.plugins.sqlDelight)
+//    alias(libs.plugins.sqlDelight)
 }
 setupAndroidLibrary()
 android {
